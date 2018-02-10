fc9c660e2456e180443c3ecb1357b52563d21be1
hoffNaN <hofman.marcin@gmail.com>
2018-02-10 10:22:39 +0100

    author string related fix

--- trainer/serializers.py
+++ trainer/serializers.py
@@ -2,7 +2,9 @@ from trainer.models import Post
 from rest_framework import serializers
 
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(many=False)
+
     class Meta:
         model = Post
         fields = ('author', 'title', 'text', 'created_date','published_date')
