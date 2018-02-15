f76621a54403687cc912c29fe16320009b79f5dc
hoffNaN <hofman.marcin@gmail.com>
2018-02-15 18:45:13 +0100

    next models added

--- trainer/models.py
+++ trainer/models.py
@@ -31,14 +31,17 @@ class BasicUser(models.Model):
     gymowner = models.ForeignKey('GymOwner', on_delete=models.CASCADE, null=True)
 
 class Trainer(models.Model):
-    qualify = models.TextField(max_length=60)
+    qualifications = models.TextField(max_length=60)
     gym = models.ManyToManyField('Gym', blank=True)
     experience = models.TextField(max_length=60)
-    specialization = models.CharField(max_length=200)
-    opinion = models.ManyToManyField()
+    specialization = models.ForeignKey('TrainType', on_delete=models.CASCADE)
+    rating = models.ManyToManyField()
+    offert = models.ForeignKey('Offer', on_delete=models.CASCADE, null=True)
 
 class GymOwner(models.Model):
     gym = models.ManyToManyField('Gym', blank=True)
+    offert = models.ForeignKey('Offer', on_delete=models.CASCADE, null=True)
+
 
 class Address(models.Model):
     country_code = models.CharField(max_length=3)
@@ -47,17 +50,48 @@ class Address(models.Model):
 
 class Contact(models.Model):
     phone = models.CharField(max_length=9)
-    email = models.CharField(max_length=25)
+    email = models.EmailField()
     fb = models.CharField(max_length=30)
 
 class Gym(models.Model):
     name_gym = models.CharField(max_length=20)
-    train_type = models.ForeignKey('TrainType', on_delete=models.CASCADE)
     discription = models.TextField(max_length=400)
     contact = models.ForeignKey('Contact', on_delete=models.CASCADE)
-    price = models.
     trainer = models.ForeignKey('Trainer', on_delete=models.CASCADE, null=True)
+    offert = models.ForeignKey('TrainType', on_delete=models.CASCADE)
 
 class TrainType(models.model):
+    JOGA = 'joga'
+    TBC = 'tbc'
+    FITNESS = 'fitness'
+    CROSSFIT = 'crossfit'
+    train_choice = (
+        (JOGA, 'joga'),
+        (TBC, 'tbc'),
+        (FITNESS, 'fitness'),
+        (CROSSFIT, 'crossfit'),
+    )
+
+    train_type = models.CharField(max_length=5, choices=train_choice, default=JOGA)
+
+class Offer(models.model):
+    discription = models.TextField(max_length=200)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    train_type = models.ForeignKey('TrainType', on_delete=models.CASCADE)
+    localization = models.ForeignKey('Address', on_delete=models.CASCADE)
 
 class Opinion(models.model):
+    WORSE = 'worse'
+    BAD = 'bad'
+    NEUTRAL = 'neutral'
+    GOOD = 'good'
+    BEST = 'best'
+    rating_choice = (
+        (WORSE, 'worse'),
+        (BAD, 'bad'),
+        (NEUTRAL, 'neutral'),
+        (GOOD, 'good'),
+        (BEST, 'best')
+    )
+
+    opinion_type = models.CharField(max_length=5, choices=rating_choice, default=NEUTRAL)
